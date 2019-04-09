2fcb4eb45a483bfb8fc5451257baed0f4fbddc63
Svitlana Filatova and Stephen Trewick <svitlana@>
2019-04-09 16:42:47 -0700

    Add business logic functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,40 @@
 
 
-// function Theater() {
-//   this.movies = [],
-//   this.currentId = 0
-// }
-//
-// Theater.prototype.assignId = function() {
-//   this.currentId += 1;
-//   return this.currentId;
+function Theater() {
+  this.movies = [],
+  this.currentId = 0
+}
 
-var movie = {
-  title: "Batman",
-  time: ["Morning"],
-  new: true,
-  rating: "PG-13"
-};
+Theater.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
 
+function Movie(moveTitle, movieTime, movieRating) {
+  this.moveTitle = moveTitle,
+  this.movieTime = movieTime,
+  this.movieRating = movieRating
+}
 
+Theater.prototype.addMovie = function (movie) {
+  movie.id = this.assignId();
+  this.movies.push(movie);
+}
 
+var titles = ["Batman", "Capitan Marvel", "Black Mother", "Buddy"];
+var time = ["Morning", "Afternoon", "Evening"];
+var rating = ["PG-13", "R"];
 
-// function() {
-//   if (age > 60) {
-//     cost -= seniorDiscount;
-//   }
-// };
+// User Interface Logic ---------
+var Theatre = new Theatre();
+
 
 
 $(document).ready(function() {
   //call function for checking if customer can purchase ticket
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var cost = 10;
+    var ticketCost = 10;
     var seniorDiscount = 3;
     var morningDiscount = 3;
 
